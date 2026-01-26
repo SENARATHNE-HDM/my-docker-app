4b288f6aacaf478d407e2aaf107c53857b5e4788
Dewmie Senarathne <dewsjgh@gmail.com>
2026-01-26 22:01:46 +0530

    My first docker app

---
+++ app.py
@@ -0,0 +1,4 @@
+print("------------------------------------------")
+print("Hello Boss! I am running inside a Docker Container")
+print("No Python needed on your machine!")
+print("------------------------------------------")
